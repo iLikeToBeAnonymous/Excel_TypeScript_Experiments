58f72fbbb9588e9e2d6c27c1696f0859a96e6aef
iLikeToBeAnonymous <nunya@busin.ess>
2022-07-22 14:18:42 -0400

    Improved the default cell formatting of a PID and moved the code into its own function. I also added this code as a standalone function in Formatting_Cells.ts, but I can't import functions from a separate file, unfortunately.

---
+++ Formatting_Cells.ts
@@ -0,0 +1,25 @@
+function main(workbook: ExcelScript.Workbook) {
+	// let selectedSheet = workbook.getActiveWorksheet();
+	/* https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.workbook?view=office-scripts#excelscript-excelscript-workbook-getselectedrange-member(1) */
+	let myRange = workbook.getSelectedRange();
+
+	formatCellRange(myRange);
+};
+
+function formatCellRange(myRange: ExcelScript.Range) {
+	/* Clear formats from range (just in case there is formatting applied other than what is being specified here.) 
+	This also clears out any manually-selected cell fill/highlighting 
+    https://docs.microsoft.com/en-us/office/dev/add-ins/excel/excel-add-ins-ranges-set-get-values */
+	myRange.clear(ExcelScript.ClearApplyTo.formats);
+
+	// Set vertical alignment to ExcelScript.VerticalAlignment.center for myRange on selectedSheet
+	myRange.getFormat().setVerticalAlignment(ExcelScript.VerticalAlignment.center);
+	myRange.getFormat().setIndentLevel(0);
+	// Set horizontal alignment to ExcelScript.HorizontalAlignment.left for range N61 on selectedSheet
+	myRange.getFormat().setHorizontalAlignment(ExcelScript.HorizontalAlignment.left);
+
+	// Set font name to "Consolas" for myRange on selectedSheet
+	myRange.getFormat().getFont().setName("Consolas");
+	// Set font size to 10 for myRange on selectedSheet
+	myRange.getFormat().getFont().setSize(10);
+};
--- Production_ID_Generator.ts
+++ Production_ID_Generator.ts
@@ -8,20 +8,32 @@ function main(workbook: ExcelScript.Workbook) {
   // scriptTarget.clear(); // This clears values and formats for the selected range.
   scriptTarget.setValue(myPid); // Sets the date code/PID into the selected cells.
   
-  /* https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.rangeformat?view=office-scripts
-     https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.verticalalignment?view=office-scripts */
-    let vertAlign: ExcelScript.VerticalAlignment = 1; // (2: bottom, 1: middle, 0: top)
-    scriptTarget.getFormat().setVerticalAlignment(vertAlign);
-  
-  // https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.rangefont?view=office-scripts
-  const cellFont = scriptTarget.getFormat().getFont();
-  cellFont.setName('Consolas');
-  cellFont.setSize(10);
+  formatRangeAsCode(scriptTarget); // Call the fixed-width font formatter function
 };
 
 function genDateTime() {
   var now = new Date();
   return now.toISOString(); // must use "val" instead of "text" since it's an input box.
+};
+
+function formatRangeAsCode(myRange: ExcelScript.Range) {
+	/* Clear formats from range (just in case there is formatting applied other than what is being specified here.) 
+	This also clears out any manually-selected cell fill/highlighting 
+    https://docs.microsoft.com/en-us/office/dev/add-ins/excel/excel-add-ins-ranges-set-get-values
+    https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.rangeformat?view=office-scripts
+    https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.verticalalignment?view=office-scripts */
+	myRange.clear(ExcelScript.ClearApplyTo.formats);
+
+	// Set vertical alignment to ExcelScript.VerticalAlignment.center for myRange on selectedSheet
+	myRange.getFormat().setVerticalAlignment(ExcelScript.VerticalAlignment.center);
+	myRange.getFormat().setIndentLevel(0);
+	// Set horizontal alignment to ExcelScript.HorizontalAlignment.left for range N61 on selectedSheet
+	myRange.getFormat().setHorizontalAlignment(ExcelScript.HorizontalAlignment.left);
+
+	// Set font name to "Consolas" for myRange on selectedSheet
+	myRange.getFormat().getFont().setName("Consolas");
+	// Set font size to 10 for myRange on selectedSheet
+	myRange.getFormat().getFont().setSize(10);
 };
 
 function convertToBase(originalNumber: string|number, targetBaseSystem: number) {

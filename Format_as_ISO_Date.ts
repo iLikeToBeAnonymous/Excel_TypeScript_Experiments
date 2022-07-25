53244b09884e0aa4043f5b829bf15bbc82926f83
iLikeToBeAnonymous <nunya@busin.ess>
2022-07-25 14:20:09 -0400

    Got a working script that only formats the cell in ISO format if the cell format comes back as category == date. However, this fails to catch custom date formats, because their category is returned as "custom."

--- Format_as_ISO_Date.ts
+++ Format_as_ISO_Date.ts
@@ -8,25 +8,39 @@ function main(workbook: ExcelScript.Workbook) {
 
 // https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.numberformatcategory?view=office-scripts#examples
 function formatAsIsoDate(myRange: ExcelScript.Range) {
-    let rangeContents = myRange.getValues();
-    let rangeFormats = myRange.getNumberFormatCategories();
+	let rangeContents = myRange.getValues();
+	let nmbrFormatCats = myRange.getNumberFormatCategories();
+    let nmbrFormatLocalCats = myRange.getNumberFormatsLocal(); // myRange.getNumberFormatLocal();
+    let nmbrFormatCodes = myRange.getNumberFormats();
 
-    rangeContents.forEach((row, rowIndx) => {
-        row.forEach((cell, cellIndx) => {
-            console.log('Cell val: ' + cell + ' has format of ' + rangeFormats[rowIndx][cellIndx])
-        });
-    });
-    
-    // let cellContents = myRange.getValues();
-    // cellContents.forEach((row,rowIndx) => {
-    //     // console.log('row val: ' + row); // Prints each row as a csv string
-    //     row.forEach((cell,cellIndx) => {
-    //         console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' = ' + cell)
-    //     });
-    // });
+	rangeContents.forEach((row, rowIndx) => {
+		row.forEach((cell, cellIndx) => {
+			// console.log('Cell val: ' + cell + ' has format of ' + nmbrFormatCats[rowIndx][cellIndx]);
+            console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' has\n\tnumberFormatCategory of ' + nmbrFormatCats[rowIndx][cellIndx] +
+                '\n\tand nmbrFormatLocalCats val of ' + nmbrFormatLocalCats[rowIndx][cellIndx] + 
+                '\n\tand nmbrFormatCode of ' + nmbrFormatCodes[rowIndx][cellIndx]);
+            if(nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.date){
+                console.log('Correcting format for cell');
+                myRange.getCell(rowIndx, cellIndx).setNumberFormatLocal("yyyy-mm-dd;@");
+            }
+            // else if (nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.custom){
+            //     console.log('nmbrFormatLocalCats is ' + nmbrFormatLocalCats[rowIndx][cellIndx]);
+            // }
+            else{console.log('Cell is not a date')}
+		});
+	});
+
+	// myRange.setNumberFormatLocal("yyyy-mm-dd;@");
+	// let cellContents = myRange.getValues();
+	// cellContents.forEach((row,rowIndx) => {
+	//     // console.log('row val: ' + row); // Prints each row as a csv string
+	//     row.forEach((cell,cellIndx) => {
+	//         console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' = ' + cell)
+	//     });
+	// });
 
 	// // let numberFormatCategories = myRange.getNumberFormatCategories();
-    // // console.log(numberFormatCategories);
+	// // console.log(numberFormatCategories);
 	// // numberFormatCategories.forEach(row => {
 	// // 		row.forEach(cell => {console.log(cell)	
 	// // 	});

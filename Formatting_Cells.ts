33ccec9fd29acb5c8383b66ba647fd7b38ff89c9
iLikeToBeAnonymous <nunya@busin.ess>
2022-07-25 10:46:20 -0400

    Added additional inline notes to Formatting_Cells.ts and added a .gitignore file.

--- Formatting_Cells.ts
+++ Formatting_Cells.ts
@@ -9,7 +9,9 @@ function main(workbook: ExcelScript.Workbook) {
 function formatCellRange(myRange: ExcelScript.Range) {
 	/* Clear formats from range (just in case there is formatting applied other than what is being specified here.) 
 	This also clears out any manually-selected cell fill/highlighting 
-    https://docs.microsoft.com/en-us/office/dev/add-ins/excel/excel-add-ins-ranges-set-get-values */
+    https://docs.microsoft.com/en-us/office/dev/add-ins/excel/excel-add-ins-ranges-set-get-values
+    https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.rangeformat?view=office-scripts
+    https://docs.microsoft.com/en-us/javascript/api/office-scripts/excelscript/excelscript.verticalalignment?view=office-scripts */
 	myRange.clear(ExcelScript.ClearApplyTo.formats);
 
 	// Set vertical alignment to ExcelScript.VerticalAlignment.center for myRange on selectedSheet

63d20e0a9656682639899ecc6db6fc749c5dfc3f
iLikeToBeAnonymous <nunya@busin.ess>
2022-07-26 16:25:08 -0400

    Modified the function so it can check if the value stored is a date, not just checking if it's in the date category. Via experimentation, I determined that a date coded in as a formula can still be accurately determined as a date, but it could also potentially bypass formatting enforcement. (e.g., if a cell contains the formula '="1/2/1970"', it WILL be detected as a date, but date formatting will not be able to override the way the formula is written.

--- Format_as_ISO_Date.ts
+++ Format_as_ISO_Date.ts
@@ -10,23 +10,42 @@ function main(workbook: ExcelScript.Workbook) {
 function formatAsIsoDate(myRange: ExcelScript.Range) {
 	let rangeContents = myRange.getValues();
 	let nmbrFormatCats = myRange.getNumberFormatCategories();
-    let nmbrFormatLocalCats = myRange.getNumberFormatsLocal(); // myRange.getNumberFormatLocal();
+    // let nmbrFormatLocalCats = myRange.getNumberFormatsLocal(); // myRange.getNumberFormatLocal();
     let nmbrFormatCodes = myRange.getNumberFormats();
-
+    let debugMsg: string;
 	rangeContents.forEach((row, rowIndx) => {
-		row.forEach((cell, cellIndx) => {
-			// console.log('Cell val: ' + cell + ' has format of ' + nmbrFormatCats[rowIndx][cellIndx]);
-            console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' has\n\tnumberFormatCategory of ' + nmbrFormatCats[rowIndx][cellIndx] +
-                '\n\tand nmbrFormatLocalCats val of ' + nmbrFormatLocalCats[rowIndx][cellIndx] + 
-                '\n\tand nmbrFormatCode of ' + nmbrFormatCodes[rowIndx][cellIndx]);
-            if(nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.date){
-                console.log('Correcting format for cell');
-                myRange.getCell(rowIndx, cellIndx).setNumberFormatLocal("yyyy-mm-dd;@");
-            }
-            // else if (nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.custom){
-            //     console.log('nmbrFormatLocalCats is ' + nmbrFormatLocalCats[rowIndx][cellIndx]);
+		row.forEach((cellVal, cellIndx) => {
+			// console.log('Cell val: ' + cellVal + ' has format of ' + nmbrFormatCats[rowIndx][cellIndx]);
+            // console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' has\n\tnumberFormatCategory of ' + nmbrFormatCats[rowIndx][cellIndx] +
+            //     '\n\tand nmbrFormatLocalCats val of ' + nmbrFormatLocalCats[rowIndx][cellIndx] + 
+            //     '\n\tand nmbrFormatCode of ' + nmbrFormatCodes[rowIndx][cellIndx]);
+            // console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' has\n\tnumberFormatCategory of ' + nmbrFormatCats[rowIndx][cellIndx] +
+            //     '\n\tand nmbrFormatCode of ' + nmbrFormatCodes[rowIndx][cellIndx]);
+            debugMsg = 'Row ' + rowIndx + ', Col ' + cellIndx + ': ' + cellVal + 
+            '\n\thas numberFormatCategory of ' + nmbrFormatCats[rowIndx][cellIndx] +
+            '\n\tand nmbrFormatCode of ' + nmbrFormatCodes[rowIndx][cellIndx];
+                if(nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.date){
+                    debugMsg += '\n\tFORMAT IS "DATE"'; //console.log('format is date')
+                    myRange.getCell(rowIndx, cellIndx).setNumberFormatLocal("yyyy-mm-dd;@");
+                }
+                // Beginning of Unix Epoc is 1970-01-01, which in Microsoft time is 25569.
+                else if((nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.custom) && cellVal > 25569){
+                    debugMsg += '\n\tCUSTOM FORMAT IS LIKELY A DATE'; //console.log('Custom format is likely a date');
+                    myRange.getCell(rowIndx, cellIndx).setNumberFormatLocal("yyyy-mm-dd;@");
+                }
+                else {
+                    debugMsg += '\n\tVALUE CANNOT BE COERCED INTO A DATE'; //console.log('Value cannot be coerced into a date')
+                };
+
+            console.log(debugMsg);
+            // if(nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.date){
+            //     console.log('Correcting format for cellVal');
+            //     myRange.getCell(rowIndx, cellIndx).setNumberFormatLocal("yyyy-mm-dd;@");
             // }
-            else{console.log('Cell is not a date')}
+            // // else if (nmbrFormatCats[rowIndx][cellIndx] == ExcelScript.NumberFormatCategory.custom){
+            // //     console.log('nmbrFormatLocalCats is ' + nmbrFormatLocalCats[rowIndx][cellIndx]);
+            // // }
+            // else{console.log('Cell is not a date')}
 		});
 	});
 
@@ -34,15 +53,15 @@ function formatAsIsoDate(myRange: ExcelScript.Range) {
 	// let cellContents = myRange.getValues();
 	// cellContents.forEach((row,rowIndx) => {
 	//     // console.log('row val: ' + row); // Prints each row as a csv string
-	//     row.forEach((cell,cellIndx) => {
-	//         console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' = ' + cell)
+	//     row.forEach((cellVal,cellIndx) => {
+	//         console.log('Row ' + rowIndx + ', Col ' + cellIndx + ' = ' + cellVal)
 	//     });
 	// });
 
 	// // let numberFormatCategories = myRange.getNumberFormatCategories();
 	// // console.log(numberFormatCategories);
 	// // numberFormatCategories.forEach(row => {
-	// // 		row.forEach(cell => {console.log(cell)	
+	// // 		row.forEach(cellVal => {console.log(cellVal)	
 	// // 	});
 	// // });
 	// // // numberFormatCategories.forEach((category, index) => {
@@ -59,3 +78,9 @@ function formatAsIsoDate(myRange: ExcelScript.Range) {
 	// // // // Set number format for range U393 on selectedSheet
 	// // // selectedSheet.getRange("U393").setNumberFormatLocal("yyyy-mm-dd;@");
 };
+
+/**
+ * d-mmm-yy
+ * [h]:mm:ss
+ * m/d/yy
+ */
